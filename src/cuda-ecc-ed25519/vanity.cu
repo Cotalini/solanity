912b96a05e0ddd3aa6dc17fdf621f76aa9e3124d
Cotali <76927007+Cotalini@users.noreply.github.com>
2024-11-05 03:58:24 -0500

    Update vanity.cu

--- src/cuda-ecc-ed25519/vanity.cu
+++ src/cuda-ecc-ed25519/vanity.cu
@@ -227,259 +227,172 @@ void __global__ vanity_init(unsigned long long int* rseed, curandState* state) {
 	curand_init(*rseed + id, id, 0, &state[id]);
 }
 
-void __global__ vanity_scan(curandState* state, int* keys_found, int* gpu, int* exec_count) {
-	int id = threadIdx.x + (blockIdx.x * blockDim.x);
-
-        atomicAdd(exec_count, 1);
-
-	// SMITH - should really be passed in, but hey ho
-    	int prefix_letter_counts[MAX_PATTERNS];
-    	for (unsigned int n = 0; n < sizeof(prefixes) / sizeof(prefixes[0]); ++n) {
-        	if ( MAX_PATTERNS == n ) {
-            		printf("NEVER SPEAK TO ME OR MY SON AGAIN");
-            		return;
-        	}
-        	int letter_count = 0;
-        	for(; prefixes[n][letter_count]!=0; letter_count++);
-        	prefix_letter_counts[n] = letter_count;
-    	}
-
-	// Local Kernel State
-	ge_p3 A;
-	curandState localState     = state[id];
-	unsigned char seed[32]     = {0};
-	unsigned char publick[32]  = {0};
-	unsigned char privatek[64] = {0};
-	char key[256]              = {0};
-	//char pkey[256]             = {0};
-
-	// Start from an Initial Random Seed (Slow)
-	// NOTE: Insecure random number generator, do not use keys generator by
-	// this program in live.
-	// SMITH: localState should be entropy random now
-	for (int i = 0; i < 32; ++i) {
-		float random    = curand_uniform(&localState);
-		uint8_t keybyte = (uint8_t)(random * 255);
-		seed[i]         = keybyte;
-	}
-
-	// Generate Random Key Data
-	sha512_context md;
-
-	// I've unrolled all the MD5 calls and special cased them to 32 byte
-	// inputs, which eliminates a lot of branching. This is a pretty poor
-	// way to optimize GPU code though.
-	//
-	// A better approach would be to split this application into two
-	// different kernels, one that is warp-efficient for SHA512 generation,
-	// and another that is warp efficient for bignum division to more
-	// efficiently scan for prefixes. Right now bs58enc cuts performance
-	// from 16M keys on my machine per second to 4M.
-	for (int attempts = 0; attempts < ATTEMPTS_PER_EXECUTION; ++attempts) {
-		// sha512_init Inlined
-		md.curlen   = 0;
-		md.length   = 0;
-		md.state[0] = UINT64_C(0x6a09e667f3bcc908);
-		md.state[1] = UINT64_C(0xbb67ae8584caa73b);
-		md.state[2] = UINT64_C(0x3c6ef372fe94f82b);
-		md.state[3] = UINT64_C(0xa54ff53a5f1d36f1);
-		md.state[4] = UINT64_C(0x510e527fade682d1);
-		md.state[5] = UINT64_C(0x9b05688c2b3e6c1f);
-		md.state[6] = UINT64_C(0x1f83d9abfb41bd6b);
-		md.state[7] = UINT64_C(0x5be0cd19137e2179);
-
-		// sha512_update inlined
-		// 
-		// All `if` statements from this function are eliminated if we
-		// will only ever hash a 32 byte seed input. So inlining this
-		// has a drastic speed improvement on GPUs.
-		//
-		// This means:
-		//   * Normally we iterate for each 128 bytes of input, but we are always < 128. So no iteration.
-		//   * We can eliminate a MIN(inlen, (128 - md.curlen)) comparison, specialize to 32, branch prediction improvement.
-		//   * We can eliminate the in/inlen tracking as we will never subtract while under 128
-		//   * As a result, the only thing update does is copy the bytes into the buffer.
-		const unsigned char *in = seed;
-		for (size_t i = 0; i < 32; i++) {
-			md.buf[i + md.curlen] = in[i];
-		}
-		md.curlen += 32;
-
-
-		// sha512_final inlined
-		// 
-		// As update was effectively elimiated, the only time we do
-		// sha512_compress now is in the finalize function. We can also
-		// optimize this:
-		//
-		// This means:
-		//   * We don't need to care about the curlen > 112 check. Eliminating a branch.
-		//   * We only need to run one round of sha512_compress, so we can inline it entirely as we don't need to unroll.
-		md.length += md.curlen * UINT64_C(8);
-		md.buf[md.curlen++] = (unsigned char)0x80;
-
-		while (md.curlen < 120) {
-			md.buf[md.curlen++] = (unsigned char)0;
-		}
-
-		STORE64H(md.length, md.buf+120);
-
-		// Inline sha512_compress
-		uint64_t S[8], W[80], t0, t1;
-		int i;
-
-		/* Copy state into S */
-		for (i = 0; i < 8; i++) {
-			S[i] = md.state[i];
-		}
-
-		/* Copy the state into 1024-bits into W[0..15] */
-		for (i = 0; i < 16; i++) {
-			LOAD64H(W[i], md.buf + (8*i));
-		}
-
-		/* Fill W[16..79] */
-		for (i = 16; i < 80; i++) {
-			W[i] = Gamma1(W[i - 2]) + W[i - 7] + Gamma0(W[i - 15]) + W[i - 16];
-		}
-
-		/* Compress */
-		#define RND(a,b,c,d,e,f,g,h,i) \
-		t0 = h + Sigma1(e) + Ch(e, f, g) + K[i] + W[i]; \
-		t1 = Sigma0(a) + Maj(a, b, c);\
-		d += t0; \
-		h  = t0 + t1;
-
-		for (i = 0; i < 80; i += 8) {
-			RND(S[0],S[1],S[2],S[3],S[4],S[5],S[6],S[7],i+0);
-			RND(S[7],S[0],S[1],S[2],S[3],S[4],S[5],S[6],i+1);
-			RND(S[6],S[7],S[0],S[1],S[2],S[3],S[4],S[5],i+2);
-			RND(S[5],S[6],S[7],S[0],S[1],S[2],S[3],S[4],i+3);
-			RND(S[4],S[5],S[6],S[7],S[0],S[1],S[2],S[3],i+4);
-			RND(S[3],S[4],S[5],S[6],S[7],S[0],S[1],S[2],i+5);
-			RND(S[2],S[3],S[4],S[5],S[6],S[7],S[0],S[1],i+6);
-			RND(S[1],S[2],S[3],S[4],S[5],S[6],S[7],S[0],i+7);
-		}
-
-		#undef RND
-
-		/* Feedback */
-		for (i = 0; i < 8; i++) {
-			md.state[i] = md.state[i] + S[i];
-		}
-
-		// We can now output our finalized bytes into the output buffer.
-		for (i = 0; i < 8; i++) {
-			STORE64H(md.state[i], privatek+(8*i));
-		}
-
-		// Code Until here runs at 87_000_000H/s.
-
-		// ed25519 Hash Clamping
-		privatek[0]  &= 248;
-		privatek[31] &= 63;
-		privatek[31] |= 64;
-
-		// ed25519 curve multiplication to extract a public key.
-		ge_scalarmult_base(&A, privatek);
-		ge_p3_tobytes(publick, &A);
-
-		// Code Until here runs at 87_000_000H/s still!
-
-		size_t keysize = 256;
-		b58enc(key, &keysize, publick, 32);
-
-		// Code Until here runs at 22_000_000H/s. b58enc badly needs optimization.
-
-		// We don't have access to strncmp/strlen here, I don't know
-		// what the efficient way of doing this on a GPU is, so I'll
-		// start with a dumb loop. There seem to be implementations out
-		// there of bignunm division done in parallel as a CUDA kernel
-		// so it might make sense to write a new parallel kernel to do
-		// this.
-
-                for (int i = 0; i < sizeof(prefixes) / sizeof(prefixes[0]); ++i) {
-
-                        for (int j = 0; j<prefix_letter_counts[i]; ++j) {
-
-				// it doesn't match this prefix, no need to continue
-				if ( !(prefixes[i][j] == '?') && !(prefixes[i][j] == key[j]) ) {
-					break;
-				}
-
-                                // we got to the end of the prefix pattern, it matched!
-                                if ( j == ( prefix_letter_counts[i] - 1) ) {
-                                        atomicAdd(keys_found, 1);
-                                        //size_t pkeysize = 256;
-                                        //b58enc(pkey, &pkeysize, seed, 32);
-                                       
-				        // SMITH	
-					// The 'key' variable is the public key in base58 'address' format
-                                        // We display the seed in hex
-
-					// Solana stores the keyfile as seed (first 32 bytes)
-					// followed by public key (last 32 bytes)
-					// as an array of decimal numbers in json format
-
-                                        printf("GPU %d MATCH %s - ", *gpu, key);
-                                        for(int n=0; n<sizeof(seed); n++) { 
-						printf("%02x",(unsigned char)seed[n]); 
-					}
-					printf("\n");
-					
-                                        printf("[");
-					for(int n=0; n<sizeof(seed); n++) { 
-						printf("%d,",(unsigned char)seed[n]); 
-					}
-                                        for(int n=0; n<sizeof(publick); n++) {
-					        if ( n+1==sizeof(publick) ) {	
-							printf("%d",publick[n]);
-						} else {
-							printf("%d,",publick[n]);
-						}
-					}
-                                        printf("]\n");
-
-					/*
-					printf("Public: ");
-                                        for(int n=0; n<sizeof(publick); n++) { printf("%d ",publick[n]); }
-					printf("\n");
-					printf("Private: ");
-                                        for(int n=0; n<sizeof(privatek); n++) { printf("%d ",privatek[n]); }
-					printf("\n");
-					printf("Seed: ");
-                                        for(int n=0; n<sizeof(seed); n++) { printf("%d ",seed[n]); }
-					printf("\n");
-                                        */
-
-                                        break;
-				}
-
-                        }
-		}
-
-		// Code Until here runs at 22_000_000H/s. So the above is fast enough.
-
-		// Increment Seed.
-		// NOTE: This is horrifically insecure. Please don't use these
-		// keys on live. This increment is just so we don't have to
-		// invoke the CUDA random number generator for each hash to
-		// boost performance a little. Easy key generation, awful
-		// security.
-		for (int i = 0; i < 32; ++i) {
-			if (seed[i] == 255) {
-				seed[i]  = 0;
-			} else {
-				seed[i] += 1;
-				break;
-			}
-		}
-	}
-
-	// Copy Random State so that future calls of this kernel/thread/block
-	// don't repeat their sequences.
-	state[id] = localState;
+__device__ char device_tolower(char c) {
+    if (c >= 'A' && c <= 'Z') {
+        return c + ('a' - 'A');
+    }
+    return c;
 }
+
+void __global__ vanity_scan(curandState* state, int* keys_found, int* gpu, int* exec_count) {
+    int id = threadIdx.x + (blockIdx.x * blockDim.x);
+
+    atomicAdd(exec_count, 1);
+
+    int prefix_letter_counts[MAX_PATTERNS];
+    for (unsigned int n = 0; n < sizeof(prefixes) / sizeof(prefixes[0]); ++n) {
+        if (MAX_PATTERNS == n) {
+            printf("NEVER SPEAK TO ME OR MY SON AGAIN");
+            return;
+        }
+        int letter_count = 0;
+        for (; prefixes[n][letter_count] != 0; letter_count++);
+        prefix_letter_counts[n] = letter_count;
+    }
+
+    ge_p3 A;
+    curandState localState = state[id];
+    unsigned char seed[32] = { 0 };
+    unsigned char publick[32] = { 0 };
+    unsigned char privatek[64] = { 0 };
+    char key[256] = { 0 };
+
+    for (int i = 0; i < 32; ++i) {
+        float random = curand_uniform(&localState);
+        uint8_t keybyte = (uint8_t)(random * 255);
+        seed[i] = keybyte;
+    }
+
+    sha512_context md;
+
+    for (int attempts = 0; attempts < ATTEMPTS_PER_EXECUTION; ++attempts) {
+        md.curlen = 0;
+        md.length = 0;
+        md.state[0] = UINT64_C(0x6a09e667f3bcc908);
+        md.state[1] = UINT64_C(0xbb67ae8584caa73b);
+        md.state[2] = UINT64_C(0x3c6ef372fe94f82b);
+        md.state[3] = UINT64_C(0xa54ff53a5f1d36f1);
+        md.state[4] = UINT64_C(0x510e527fade682d1);
+        md.state[5] = UINT64_C(0x9b05688c2b3e6c1f);
+        md.state[6] = UINT64_C(0x1f83d9abfb41bd6b);
+        md.state[7] = UINT64_C(0x5be0cd19137e2179);
+
+        const unsigned char* in = seed;
+        for (size_t i = 0; i < 32; i++) {
+            md.buf[i + md.curlen] = in[i];
+        }
+        md.curlen += 32;
+
+        md.length += md.curlen * UINT64_C(8);
+        md.buf[md.curlen++] = (unsigned char)0x80;
+
+        while (md.curlen < 120) {
+            md.buf[md.curlen++] = (unsigned char)0;
+        }
+
+        STORE64H(md.length, md.buf + 120);
+
+        uint64_t S[8], W[80], t0, t1;
+        int i;
+
+        for (i = 0; i < 8; i++) {
+            S[i] = md.state[i];
+        }
+
+        for (i = 0; i < 16; i++) {
+            LOAD64H(W[i], md.buf + (8 * i));
+        }
+
+        for (i = 16; i < 80; i++) {
+            W[i] = Gamma1(W[i - 2]) + W[i - 7] + Gamma0(W[i - 15]) + W[i - 16];
+        }
+
+#define RND(a, b, c, d, e, f, g, h, i) \
+        t0 = h + Sigma1(e) + Ch(e, f, g) + K[i] + W[i]; \
+        t1 = Sigma0(a) + Maj(a, b, c); \
+        d += t0; \
+        h = t0 + t1;
+
+        for (i = 0; i < 80; i += 8) {
+            RND(S[0], S[1], S[2], S[3], S[4], S[5], S[6], S[7], i + 0);
+            RND(S[7], S[0], S[1], S[2], S[3], S[4], S[5], S[6], i + 1);
+            RND(S[6], S[7], S[0], S[1], S[2], S[3], S[4], S[5], i + 2);
+            RND(S[5], S[6], S[7], S[0], S[1], S[2], S[3], S[4], i + 3);
+            RND(S[4], S[5], S[6], S[7], S[0], S[1], S[2], S[3], i + 4);
+            RND(S[3], S[4], S[5], S[6], S[7], S[0], S[1], S[2], i + 5);
+            RND(S[2], S[3], S[4], S[5], S[6], S[7], S[0], S[1], i + 6);
+            RND(S[1], S[2], S[3], S[4], S[5], S[6], S[7], S[0], i + 7);
+        }
+
+#undef RND
+
+        for (i = 0; i < 8; i++) {
+            md.state[i] = md.state[i] + S[i];
+        }
+
+        for (i = 0; i < 8; i++) {
+            STORE64H(md.state[i], privatek + (8 * i));
+        }
+
+        privatek[0] &= 248;
+        privatek[31] &= 63;
+        privatek[31] |= 64;
+
+        ge_scalarmult_base(&A, privatek);
+        ge_p3_tobytes(publick, &A);
+
+        size_t keysize = 256;
+        b58enc(key, &keysize, publick, 32);
+
+        for (int i = 0; i < sizeof(prefixes) / sizeof(prefixes[0]); ++i) {
+            for (int j = 0; j < prefix_letter_counts[i]; ++j) {
+                char prefix_char = device_tolower(prefixes[i][j]);
+                char key_char = device_tolower(key[j]);
+
+                if (!(prefix_char == '?' || prefix_char == key_char)) {
+                    break;
+                }
+
+                if (j == (prefix_letter_counts[i] - 1)) {
+                    atomicAdd(keys_found, 1);
+
+                    printf("GPU %d MATCH %s - ", *gpu, key);
+                    for (int n = 0; n < sizeof(seed); n++) {
+                        printf("%02x", (unsigned char)seed[n]);
+                    }
+                    printf("\n");
+
+                    printf("[");
+                    for (int n = 0; n < sizeof(seed); n++) {
+                        printf("%d,", (unsigned char)seed[n]);
+                    }
+                    for (int n = 0; n < sizeof(publick); n++) {
+                        if (n + 1 == sizeof(publick)) {
+                            printf("%d", publick[n]);
+                        } else {
+                            printf("%d,", publick[n]);
+                        }
+                    }
+                    printf("]\n");
+
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < 32; ++i) {
+            if (seed[i] == 255) {
+                seed[i] = 0;
+            } else {
+                seed[i] += 1;
+                break;
+            }
+        }
+    }
+
+    state[id] = localState;
+}
+
 
 bool __device__ b58enc(
 	char    *b58,
